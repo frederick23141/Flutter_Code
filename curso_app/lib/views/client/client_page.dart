94c4345273997fb32cb9e83f4dca3bb41acc49b1
Federico Toro Restrepo <federicotoro23141@gmail.com>
2025-05-12 16:52:37 -0500

    .

--- curso_app/lib/views/client/client_page.dart
+++ curso_app/lib/views/client/client_page.dart
@@ -40,7 +40,7 @@ class _ClientState extends State<ClientPage> {
           ),
           style: TextStyle(color: AppColors.textPrimary),
         ),
-        backgroundColor: AppColors.buttonPrimary,
+        backgroundColor: AppColors.backgroundAlt,
       ),
       drawer: Drawer(child: DrawerMenu()),
       body: SafeArea(
